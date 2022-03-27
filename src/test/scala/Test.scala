5632af007afd3881428517db4c74261a1937faf8
pigeon_gugugu <892197240@qq.com>
2022-03-27 23:29:41 +0800

    feat: add.gitignore

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,3 +1,11 @@
-@main def test1()={
+import java.net.URI
+import java.net.http.HttpRequest.BodyPublisher
+import java.net.http.{HttpClient, HttpRequest, HttpResponse}
 
+@main def test1() = {
+  val client = HttpClient.newBuilder()
+      .build()
+  val request = HttpRequest.newBuilder().uri(URI.create("http://www.baidu.com")).build()
+  val response = client.send(request,HttpResponse.BodyHandlers.ofString())
+  println(response.toString)
 }
